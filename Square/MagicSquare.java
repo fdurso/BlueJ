b4743bdfb2a8e435ff68d4a99dcbee2b78f7c83f
crapman5389 <fjdurso3@gmail.com>
2021-04-07 09:49:02 -0400

    no idea

---
+++ Square/MagicSquare.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class MagicSquare here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MagicSquare
+{
+    private int[][] square;
+    private int n; //rows of square
+    
+    public MagicSquare(int[][] squareArray)
+    {
+        square = squareArray;
+        //initilizing n
+        n = square.length;
+    }
+}
---
+++ Square/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main()
+    {
+        int[][] magicSquare = {{16,3,2,13},{5,10,11,8},{9,6,7,12},{4,15,14,1}};
+        
+        MagicSquare testArray = new MagicSquare(magicSquare);
+    }
+}
